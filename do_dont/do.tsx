bf96d3dd5ff640d3dbcffe5927b8c180e206595f
Edward Wong <edwardlm.wong@gmail.com>
2020-08-03 17:21:13 -0400

    Do's don'ts.

--- do_dont/do.tsx
+++ do_dont/do.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
-// Do
-// 1. Only call useSate (and any Hook, in general) at the top level
-// 2. Call useState (and any Hook, in general) from React function components.
-// 3. Call useState (and any Hook, in general) from custom Hooks.
+// Do's
+// Let's first go over the do's for useState.
+// Note that this also applies to all react hooks.
+// 1. Only call useState at the top level.
+// 2. Call useState from React functional components.
+// 3. Call useState from custom Hooks.
 
 // React function component A
 const HelloDo1: React.FC = () => {
@@ -17,6 +19,12 @@ function HelloDo2() {
   return <div>{hello}</div>;
 };
 
+// So for functional components, you can declare them as shown with HelloDo1 and HelloDo2.
+// Both are equivalent ways of creating a functional component.
+// The name of the component needs to start with a capital letter.
+// When you call useState, it returns a state value as well as its value setter.
+// Note, that you need to feed in an starting value to useState.
+
 interface IState {
   hello: string;
 }
@@ -26,3 +34,10 @@ const useHelloHook = (init: IState) => {
   const [hello, setHello] = useState(init);
   return [hello, setHello];
 };
+
+// Finally, here is an example of how to create a custom hook.
+// The name of the hook needs to start with lower cased use.
+// You have to take in an initial state, where you define the interface.
+// In this case, our IState interface only has the hello string property.
+// It then returns, the state value as well as the setter.
+// Later in the video we'll create a form hook that will greatly simplify form handling.
--- do_dont/dont.tsx
+++ do_dont/dont.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
-// Don't
+// Don'ts
+
+// Now for the don'ts.
 
 // Not top level
-const HelloDo: React.FC = () => {
+const HelloDont: React.FC = () => {
   const c1 = () => {
     const [hello1, setHello1] = useState("hi");
   };
@@ -14,6 +16,12 @@ const HelloDo: React.FC = () => {
   return <div></div>;
 };
 
+// We'll start with a functional component where useState is not called at the top level.
+// We see two examples of this.
+// The first one is when useState is called in a nested function.
+// And the second is when it's called in an if statement.
+// Both leads to linting errors, seen with the red underline.
+
 // Not functional component or custom hook
 const c1 = () => {
   const [hello, setHello] = useState("hi");
@@ -22,3 +30,9 @@ const c1 = () => {
 function c2() {
   const [hello, setHello] = useState("hi");
 }
+
+// Finally, we see useState called inside a function that is neither a functional component or a custom hook.
+// In this case the linter gives an error because c1 and c2 doesn't begin with the lowercase use word
+// or a capital.
+
+// Ok, now that we know how to use useState properly, let's get to the coding example.
